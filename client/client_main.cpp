debfa44426bbefae0e2b9dff8a79dc8e0cc979eb
lyf <1305809445@qq.com>
2022-10-27 11:43:08 +0800

    change log

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -12,15 +12,13 @@ int main() {
 	char file_path[128];//文件名
 	char buffer[BUFFER_SIZE];//保存发送的数据
 	int Result;//保存返回值
-	int last_error;//保存错误码
 	//启动Winsocket
 	WSADATA wsaData;
 	Result = WSAStartup(0x0101, &wsaData);
 	if (Result)
 	{
 		printf("WSAStartup failed with error: %d", Result);
-		last_error = WSAGetLastError();
-		fprintf(log_file, "ERROR无法启动Winsocket 错误码:%d %s", last_error,asctime(localtime(&(t = time(NULL)))));
+		fprintf(log_file, "ERROR无法启动Winsocket 错误码:%d %s", WSAGetLastError(),asctime(localtime(&(t = time(NULL)))));
 		return 0;
 	}
 	fprintf(log_file, "启动Winsocket %s", asctime(localtime(&(t = time(NULL)))));
@@ -29,8 +27,7 @@ int main() {
 	client_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (client_sock == INVALID_SOCKET) {
 		printf("创建套接字失败\n");
-		last_error = WSAGetLastError();
-		fprintf(log_file, "ERROR:无法创建套接字 错误码:%d %s", last_error,asctime(localtime(&(t = time(NULL)))));
+		fprintf(log_file, "ERROR:无法创建套接字 错误码:%d %s", WSAGetLastError(),asctime(localtime(&(t = time(NULL)))));
 		return 0;
 	}
 	fprintf(log_file, "创建套接字 %s", asctime(localtime(&(t = time(NULL)))));
@@ -53,8 +50,7 @@ int main() {
 	Result = ioctlsocket(client_sock, FIONBIO, &Opt);
 	if (Result == SOCKET_ERROR) {
 		printf("设置非阻塞模式失败\n");
-		last_error = WSAGetLastError();
-		fprintf(log_file, "ERROR:无法设置非阻塞模式 错误码:%d %s", last_error,asctime(localtime(&(t = time(NULL)))));
+		fprintf(log_file, "ERROR:无法设置非阻塞模式 错误码:%d %s", WSAGetLastError(),asctime(localtime(&(t = time(NULL)))));
 		return 0;
 	}
 	//绑定客户端接口
@@ -63,8 +59,7 @@ int main() {
 	{
 		// 绑定失败
 		printf("Client socket bind error!\n");
-		last_error = WSAGetLastError();
-		fprintf(log_file, "ERROR:无法绑定接口 错误码:%d %s", last_error, asctime(localtime(&(t = time(NULL)))));
+		fprintf(log_file, "ERROR:无法绑定接口 错误码:%d %s", WSAGetLastError(), asctime(localtime(&(t = time(NULL)))));
 		return 0;
 	}
 	//if (scanf("%s", file_path) == NULL) {
--- client/upload.cpp
+++ client/upload.cpp
@@ -8,7 +8,7 @@ void upload(int mode, const char* filename, char* buffer, SOCKET sock, sockaddr_
 	int data_size;//每次成功从文件读取的字节数
 	int block_num = 0;//传送序号
 	char data[DATA_SIZE];//读取文件数据
-	char recv_buffer[BUFFER_SIZE];
+	char recv_buffer[BUFFER_SIZE];//保存接收数据
 	BOOL send_flag = FALSE;//传输是否完成
 	FILE* fp;
 	fp = fopen(filename, "r");
@@ -19,8 +19,9 @@ void upload(int mode, const char* filename, char* buffer, SOCKET sock, sockaddr_
 	write_request(mode, filename, buffer, sock, addr, addrlen);
 	while (1) {
 		result = receive_ACK(recv_buffer, sock, serveraddr, addrlen);
+		//收到正确ACK包
 		if (result == 1) {
-			max_send = 0;
+			max_send = 0;//重置重传次数
 			if (send_flag) {
 				printf("传输完毕\n");
 				return;
@@ -31,23 +32,31 @@ void upload(int mode, const char* filename, char* buffer, SOCKET sock, sockaddr_
 			data_size = fread(data, 1, DATA_SIZE, fp);
 			printf("读取文件数据size:%d\n", data_size);
 			result = send_data(sock, serveraddr, addrlen, fp, buffer, data, data_size, ++block_num);
-			if (data_size < 512) {
-				send_flag = TRUE;
+			if (data_size < 512 && result != -1) {
+				send_flag = TRUE;//传输完毕
 			}
 		}
-		//超时重传
+		//超时或发送失败重传
 		else if (result == -1) {
 			max_send++;
 			if (max_send > MAX_RETRANSMISSION) {
 				printf("重传失败\n");
+				fprintf(log_file, "ERROR:重传次数过多 %s", asctime(localtime(&(t = time(NULL)))));
 				return;
 			}
-			if (block_num > 0)
-				send_data(sock, serveraddr, addrlen, fp, buffer, data, data_size, block_num + 1);
-			else
-				return;
+			if (block_num > 0) {
+				fprintf(log_file, "重传数据包 数据包序号:%d %s", block_num, asctime(localtime(&(t = time(NULL)))));
+				send_data(sock, serveraddr, addrlen, fp, buffer, data, data_size, block_num);
+			}
+			else {
+				fprintf(log_file, "重传写请求 %s", asctime(localtime(&(t = time(NULL)))));
+				write_request(mode, filename, buffer, sock, addr, addrlen);
+			}
 		}
-		else return;
+		//收到错误包
+		else {
+			return;
+		}
 	}
 }
 //发送写请求
@@ -74,9 +83,12 @@ int write_request(int mode, const char* filename, char* buffer, SOCKET sock, soc
 	result = sendto(sock, buffer, send_size, 0, (struct sockaddr*)&addr, addrlen);
 	if (result == SOCKET_ERROR) {
 		printf("发送写请求失败\n");
+		fprintf(log_file, "ERROR:发送写请求失败 错误码:%d %s", WSAGetLastError(), asctime(localtime(&(t = time(NULL)))));
 	}
-	else
+	else {
 		printf("发送写请求成功send:%dbytes\n", result);
+		fprintf(log_file, "发送写请求成功 %s", asctime(localtime(&(t = time(NULL)))));
+	}
 	return result;
 }
 //接收数据
@@ -89,22 +101,25 @@ int receive_ACK(char* recv_buffer, SOCKET sock, sockaddr_in& addr, int addrlen) 
 	for (wait_time = 0; wait_time < TIME_OUT; wait_time++) {
 		FD_ZERO(&readfds);
 		FD_SET(sock, &readfds);
-		tv.tv_sec = 4;
+		tv.tv_sec = 2;
 		tv.tv_usec = 0;
 		select(sock + 1, &readfds, NULL, NULL, &tv);
 		result = recvfrom(sock, recv_buffer, 4, 0, (struct sockaddr*)&addr, (int*)&addrlen);
 		if (result > 0 && result < 4) {
 			printf("bad packet\n");
+			fprintf(log_file, "ERROR:接收包不正确 %s", asctime(localtime(&(t = time(NULL)))));
 			return 0;
 		}
 		else if (result >= 4) {
 			if (recv_buffer[1] == ERROR_CODE) {
 				printf("ERROR!\n");
+				fprintf(log_file, "接收到错误包 错误码:%d %s %s", recv_buffer[3], recv_buffer+4,asctime(localtime(&(t = time(NULL)))));
 				return -2;
 			}
 			printf("接收报文成功");
 			printf("recv:%dbytes ", result);
 			printf("blocknum:%d\n", recv_buffer[3]);
+			fprintf(log_file, "接收成功 包类型:%d 序号:%d %s", recv_buffer[1],recv_buffer[3],asctime(localtime(&(t = time(NULL)))));
 			return 1;
 		}
 	}
@@ -129,10 +144,12 @@ int send_data(SOCKET sock, sockaddr_in addr, int addrlen, FILE* fp, char* buffer
 	result = sendto(sock, buffer, send_size, 0, (struct sockaddr*)&addr, addrlen);
 	if (result == SOCKET_ERROR) {
 		printf("发送数据失败\n");
+		fprintf(log_file, "ERROR:发送数据失败 数据包序号:%d 错误码:%d %s", block_num,WSAGetLastError(),asctime(localtime(&(t = time(NULL)))));
 		return -1;
 	}
 	else {
 		printf("发送数据成功send:%dbytes\n", result);
+		fprintf(log_file, "发送数据包成功 数据包序号:%d %s", block_num, asctime(localtime(&(t = time(NULL)))));
 		return result;
 	}
 }
